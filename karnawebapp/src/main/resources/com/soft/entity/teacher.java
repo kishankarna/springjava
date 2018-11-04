61a42a45255ec5a1a3abd82efd792f1a8bf9c19f
karna <kisanthapamagar@gmail.com>
2018-11-03 19:19:37 -0500

    First commit

---
+++ karnawebapp/src/main/resources/com/soft/entity/teacher.java
@@ -0,0 +1,11 @@
+class Teacher{
+	
+	
+	private Integer Id;
+	private String name;
+	private String address;
+	private number phone;
+	private String department;
+	
+	
+}
---
+++ karnawebapp/target/classes/com/soft/entity/student.java
@@ -0,0 +1,7 @@
+class Student{
+	
+	private Integer Id;
+	private String name;
+	private String address;
+	private Integer grade;
+}
